aa1baae656a499c4ce49d04a5167827970b13fa6
schmitz1193 <schmitz1193@gmail.com>
2016-02-25 15:30:21 -0600

    query with input

--- app.js
+++ app.js
@@ -1,21 +1,81 @@
 'use strict';
 
+const express = require('express');
 const sqlite3 = require('sqlite3');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
 const db = new sqlite3.Database('./db/Chinook.sqlite');
 
 // db.all('SELECT * FROM employee', (err, res) => {
 //   console.log(res);
 // });
 
-db.all(`
-  SELECT   COUNT(*) AS count,
-           BillingCountry AS COUNTRY
-  FROM     Invoice
-  GROUP BY BillingCountry
-  ORDER BY count DESC`,
-    (err, res) => {
-      if (err) throw err;
+app.get('/sales-per-year', (req, res) => {
+  // How many Invoices were there in 2009 and 2011? What are the respective total sales for each of those years?
+  //req.query={filter: {year: '2009,2011"'}}
+  let having = '';
 
-      console.log(res);
-    }
-);
+  if (req.query.filter) {
+    having = 'HAVING';
+
+    req.query.filter.year
+      .split(',')
+      .map(y => +y)
+      .forEach(y => {
+        having += ` year = "${y}" OR`;
+      });
+
+    having = having.substring(0, having.length - 3);
+  }
+
+db.all(`
+    SELECT count(*) as invoices,
+           sum(Total) as total,
+           strftime('%Y', InvoiceDate) as year
+    FROM   Invoice
+    GROUP BY year
+    ${having}`,
+      (err, data) => {
+        if (err) throw err;
+
+       const roundedData = data.map(function (obj) {
+          return {
+            invoices: obj.invoices,
+            year: +obj.year,
+            total: +obj.total.toFixed(2)
+          }
+        });
+
+        res.send({
+          data: roundedData,
+          info: '# of invoices and sales per year'
+        });
+      }
+    );
+});
+
+
+app.get('/invoices-per-country', (req, res) => {
+  db.all(`
+    SELECT   COUNT(*) AS count,
+             BillingCountry AS country
+    FROM     Invoice
+    GROUP BY BillingCountry
+    ORDER BY count DESC`,
+      (err, data) => {
+        if (err) throw err;
+
+        res.send({
+          data: data,
+          info: '# of invoices per country'
+        });
+      }
+  );
+});
+
+
+app.listen (PORT, () => {
+  console.log(`Express server listening on port: ${PORT}`);
+})
